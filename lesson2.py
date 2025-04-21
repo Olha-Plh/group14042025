f95cc8336980571925194c67d2efc15db878cb39
Olha-Plh <olyaasya14@gmail.com>
2025-04-21 20:16:18 +0300

    lesson

--- lesson2.py
+++ lesson2.py
@@ -1,14 +1,28 @@
-print(id('jenjnnn'))
+# print(id("jenjnnn"))
 
 
-some_string = 'jenjnnn23'
+some_string = "jenjnnn23 \U00002763"
+some_string = "jenjnnn23 \n \U00002763"
 print(some_string)
 
-pos_number_of_a = ord('a')
-print (pos_number_of_a)
+pos_number_of_a = ord("a")
+print(pos_number_of_a)
 
 
 char_97 = chr(97)
 print(char_97)
 
-'djjdbnc'
+print(f"{char_97=}")
+formatted_string = f"{char_97=}"
+formatted_string = f"{char_97=} but {pos_number_of_a}"
+print(formatted_string)
+
+pass
+
+
+name = "Kate"
+service = "Dentist"
+address = "Beautiful, 17"
+# sms = name + service + address
+# sms = "Dear" + name + ", we're waiting for you on " + service + "our address:" + address )
+sms = f"Dear {name}, we are waiting for you on {service}. Our address: {address}"
---
+++ lesson3.py
@@ -0,0 +1,37 @@
+some_string = "am\n5     my name is Igor LLLLL 82872728 namer"
+
+
+#
+# upper = some_string.upper()
+# lower = some_string.lower()
+# title = some_string.title()
+# capitalize = some_string.capitalize()
+# chain = some_string.lower().upper().capitalize()
+#
+# clear_string_spaces = some_string.strip()
+# clear_string_symbols = some_string.strip(" 54namE")
+# clear_string_symbols_left = some_string.lstrip(" 54namE")
+# clear_string_symbols_right = some_string.rstrip(" 54namE")
+#
+# change_inner_text = (
+#     some_string.replace("name", "surname", 1)
+#     .replace("Igor", "igor")
+#     .replace("namer", "")
+#     .replace("8", "7")
+# )
+#
+# table = str.maketrans("78", "87", "\n")
+# result_smart_change = some_string.translate(table)
+
+
+slices1 = some_string[:]
+slices2 = some_string[0:12]
+slices3 = some_string[12:20]
+slices4 = some_string[::2]
+slices5 = some_string[1:25:2]
+slices6 = some_string[::-1]
+slices7 = some_string[::-2]
+slices8 = some_string[-3:-20:-2]
+
+
+pass
