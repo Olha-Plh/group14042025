13de71c277a2e2df44944c87a465da5ff8bc7b75
Olha-Plh <olyaasya14@gmail.com>
2025-04-27 21:54:07 +0300

    hw_les2.py

---
+++ hw_les2.py
@@ -0,0 +1,25 @@
+number1 = int(input('Enter a four-digit integer from the keyboard:'))
+
+
+print(number1 // 1000)
+print((number1 // 100) % 10)
+print((number1 // 10) % 10)
+print(number1 % 10)
+
+pass
+
+# second version
+number2 = int(input('Enter a four-digit integer from the keyboard:'))
+a, number2 = divmod(number2, 1000)
+print(a)
+a, number2 = divmod(number2, 100)
+print(a)
+a, number2 = divmod(number2, 10)
+print(a)
+print(number2)
+
+
+
+
+
+pass
