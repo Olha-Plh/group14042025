3a1a9f7f4435edb789d9cbd47e392c3a768109b8
Olha-Plh <olyaasya14@gmail.com>
2025-04-28 18:29:17 +0300

    Home work 2.2
    Signed-off-by: Olha-Plh <olyaasya14@gmail.com>

--- hw_les2.py
+++ hw_les2.py
@@ -1,6 +1,5 @@
 number1 = int(input('Enter a four-digit integer from the keyboard:'))
 
-
 print(number1 // 1000)
 print((number1 // 100) % 10)
 print((number1 // 10) % 10)
@@ -17,9 +16,5 @@ print(a)
 a, number2 = divmod(number2, 10)
 print(a)
 print(number2)
-
-
-
-
 
 pass
--- hw_les2_2.py
+++ hw_les2_2.py
@@ -1,12 +1,10 @@
 number1 = int(input('Enter a four-digit integer from the keyboard:'))
 
-
 digit1 = number1 % 10
 digit2 = (number1 // 10) % 10
 digit3 = (number1 // 100) % 10
 digit4 = (number1 // 1000) % 10
 digit5 = (number1 // 10000)
 reversed_number =digit1 * 10000 + digit2 * 1000 + digit3 * 100 + digit4 * 10 + digit5
-
 
 pass
