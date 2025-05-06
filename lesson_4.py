17808c89bffbc4fb70026e13f5243c032f06f3a4
Olha-Plh <olyaasya14@gmail.com>
2025-05-06 22:11:10 +0300

    lesson4_bool_values.py

---
+++ lesson_4.py
@@ -0,0 +1,51 @@
+some_string = "I live in Odessa since 2004"
+other_result = some_string.split("i")
+other_result_visual = ["I", "live", "in", "Odessa", "since", "2004"]
+
+empty_list = []
+
+if empty_list:
+    print(5555555)
+
+not_empty_list = ["4544", 5544, 5.5, True, False, [55], empty_list]
+if not_empty_list:  # отримуємо, тому що є хоч 1 елемент
+    print(222222)
+
+fifth_elem = not_empty_list[4]  # 5 елемент
+# big_elem = not_empty_list[40]
+fifth_letter = some_string[4]  # 4 індекс по порядку
+len_list = len(not_empty_list)
+len_list2 = len(empty_list)
+len_string = len(some_string)
+
+##########
+purchase_plan = ["banana"]
+
+# add 1 elem
+purchase_plan.append("salt")
+purchase_plan.append("salt")
+purchase_plan.append("2")
+
+# merge by another list
+sister_plan = ["bread", "milk"]
+purchase_plan.extend(sister_plan)
+
+purchase_plan.sort()
+purchase_plan.sort(reverse=True)
+purchase_plan.sort(key=len, reverse=True)  # число 2 перемістили в кінець з середини
+
+# delete item
+purchase_plan.remove("salt")
+
+# delete by index
+purchase_plan.pop()  # відобразили останній доданий елемент
+purchase_plan.pop(0)  # відобразили перший доданий елемент
+
+
+if "cake" in purchase_plan:
+    purchase_plan.remove("cake")
+
+if "abc" in "abcde":
+    print(32323232)
+
+pass
