6d2d2f9d32388016d75fef4de3a6db9e4d790027
Olha-Plh <olyaasya14@gmail.com>
2025-05-06 21:43:11 +0300

    betterment
    Signed-off-by: Olha-Plh <olyaasya14@gmail.com>

---
+++ shop_clients.py
@@ -0,0 +1,17 @@
+CLIENTS = [
+    "5656",
+    "dilugi5656",
+    "5656665",
+    "hjhkhkj878%^%^",
+    "5656",
+    "dilugi5656",
+    "5656665",
+    "hjhkhkj878%^%^",
+    "22ff",
+    "5656",
+    "dilugi5656",
+    "5656665",
+    "hjhkhkj878%^%^",
+]
+
+OPEN_DOOR_DAY = False
