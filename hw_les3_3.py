408be95c03543537809fe48b11ef11b4e28b2587
Olha-Plh <olyaasya14@gmail.com>
2025-05-08 06:17:16 +0300

    betterment
    Signed-off-by: Olha-Plh <olyaasya14@gmail.com>

--- hw_les3_3.py
+++ hw_les3_3.py
@@ -6,17 +6,14 @@
 # for batch in batched("123456", 6):
 #     print(batch)
 
-# my_list = [1, 2, 3, 4, 5, 6]
-# my_list = [1, 2, 3]
+# some_list = [1, 2, 3, 4, 5, 6]
+# some_list = [1, 2, 3]
 
 some_list = [1, 2, 3, 4, 5]
-# my_list = [1]
-# my_list = []
+# some_list = [1]
+# some_list = []
 some_string = some_list
 list_from_string = list(some_string)
-
-elements_in_some_string = len(some_string)
-elements_in_some_list = len(list_from_string)
 
 slice_data = list_from_string[0:]
 
