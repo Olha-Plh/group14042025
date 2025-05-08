7f9742359b64a601ea4922acb84a61825162f392
Olha-Plh <olyaasya14@gmail.com>
2025-05-08 06:22:38 +0300

    betterment
    Signed-off-by: Olha-Plh <olyaasya14@gmail.com>

--- lesson4_1.py
+++ lesson4_1.py
@@ -6,4 +6,10 @@ elements_in_some_string = len(some_string)
 elements_in_some_list = len(list_from_string)
 
 slice_data = list_from_string[0:]
+
+# All
+list_with_data = [22, 555, 66, 55]
+all_true = all(list_with_data)  # перевірка, чи всі елементи правдоподібні
+at_least_one = any(list_with_data)  # перевірка, чи хоч один елемент істинний
+
 pass
