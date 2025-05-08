eae50852738860e5f2edee8fb923d763d18a56ba
Olha-Plh <olyaasya14@gmail.com>
2025-05-08 06:31:02 +0300

    betterment
    Signed-off-by: Olha-Plh <olyaasya14@gmail.com>

--- lesson4_1.py
+++ lesson4_1.py
@@ -12,4 +12,12 @@ list_with_data = [22, 555, 66, 55]
 all_true = all(list_with_data)  # перевірка, чи всі елементи правдоподібні
 at_least_one = any(list_with_data)  # перевірка, чи хоч один елемент істинний
 
+maximum = max(some_list)
+minimum = min(some_list)
+summa = sum(some_list)
+summa1 = sum(some_list, start=10)  # start - мінімальньна сума
+
+some_str_list = ["ddhbshdddddwh", "dunxiki", "kokl"]
+max_str = max(some_str_list, key=len)  # key -функція для визначення довжини
+min_str = min(some_str_list, key=len)
 pass
