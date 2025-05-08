2f189904b4d35d8544e144382111a7e684cbfd12
Olha-Plh <olyaasya14@gmail.com>
2025-05-08 06:45:42 +0300

    betterment
    Signed-off-by: Olha-Plh <olyaasya14@gmail.com>

--- hw_les3_3.py
+++ hw_les3_3.py
@@ -1,21 +1,29 @@
-# my_list = [1, 2, 3, 4, 5, 6]
-# # my_list = [1, 2, 3]
-# # my_list = [1, 2, 3, 4, 5]
-# # my_list = [1]
-# # my_list = []
-# for batch in batched("123456", 6):
-#     print(batch)
+from itertools import batched
 
-# some_list = [1, 2, 3, 4, 5, 6]
-# some_list = [1, 2, 3]
+my_list = [1, 2, 3, 4, 5, 6]
+# my_list = [1, 2, 3]
+# my_list = [1, 2, 3, 4, 5]
+# my_list = [1]
+# my_list = []
+for batch in batched("123456", 6):
+    print(batch)
 
+some_list = [1, 2, 3, 4, 5, 6]
+some_list = [1, 2, 3]
 some_list = [1, 2, 3, 4, 5]
-# some_list = [1]
-# some_list = []
+some_list = [1]
+some_list = []
 some_string = some_list
 list_from_string = list(some_string)
-
 slice_data = list_from_string[0:]
 
+if len(some_list) == 0:
+    result = [[], []]  # нічого не робити
+elif len(some_list) % 2 == 0:  # чотна кількість
+    half = len(some_list) // 2
+    result = [some_list[:half], some_list[half:]]
+else:  # якщо не чотна кількість
+    half = len(some_list) // 2 + 1
+    result = [some_list[:half], some_list[half:]]
 
 pass
