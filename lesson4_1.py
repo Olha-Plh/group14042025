d377976bc6323001dfef6847f8cda7408dceb5e9
Olha-Plh <olyaasya14@gmail.com>
2025-05-08 06:15:23 +0300

    betterment
    Signed-off-by: Olha-Plh <olyaasya14@gmail.com>

--- lesson4_1.py
+++ lesson4_1.py
@@ -2,5 +2,8 @@ some_list = [55, 666] * 6
 some_string = "133xjbjxk"
 list_from_string = list(some_string)
 
+elements_in_some_string = len(some_string)
+elements_in_some_list = len(list_from_string)
 
+slice_data = list_from_string[0:]
 pass
