27f67afd8e561128d50dfa47a42b986c0bec5d6b
Olha-Plh <olyaasya14@gmail.com>
2025-05-08 17:55:37 +0300

    betterment
    Signed-off-by: Olha-Plh <olyaasya14@gmail.com>

--- hw_les3_3.py
+++ hw_les3_3.py
@@ -1,13 +1,15 @@
+# варіант 1
 from itertools import batched
 
-my_list = [1, 2, 3, 4, 5, 6]
-# my_list = [1, 2, 3]
+# my_list = [1, 2, 3, 4, 5, 6]
+my_list = [1, 2, 3]
 # my_list = [1, 2, 3, 4, 5]
 # my_list = [1]
 # my_list = []
-for batch in batched("123456", 6):
-    print(batch)
 
+print(list(batched(my_list, (int(len(my_list) + 1) // 2))))
+
+# варіант 2
 some_list = [1, 2, 3, 4, 5, 6]
 some_list = [1, 2, 3]
 some_list = [1, 2, 3, 4, 5]
@@ -25,6 +27,5 @@ elif len(some_list) % 2 == 0:  # чотна кількість
 else:  # якщо не чотна кількість
     half = len(some_list) // 2 + 1
     result = [some_list[:half], some_list[half:]]
-
 
 pass
