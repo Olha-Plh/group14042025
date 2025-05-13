6f34faaad345fabafc2b5de693e0cca9807a4e6e
Olha-Plh <olyaasya14@gmail.com>
2025-05-13 06:49:29 +0300

    lesson4_loops.py

--- lesson4_loops.py
+++ lesson4_loops.py
@@ -1,3 +1,7 @@
+from pprint import pprint
+
+# відображення красивого коду, а не списку-колбаси
+
 some_list = [55, 666] * 6
 some_string = ",dfngkf8dgfgikhdfkighfdikgf"
 products = ["banana", "vodka", "milk", "bread", "vodka"]
@@ -21,24 +25,64 @@ for product in products:
 print(8888888)
 
 people = [
-    ["Alex" "Bush", "Odesa", 35, True, 12131],
-    ["Petr" "Bray", "Odesa", 35, True, 46736],
-    ["Alex" "Bush", "Kyiv", 65, False, 36738],
-    ["Alex" "Bush", "Odesa", 35, True, 366772],
-    ["Petr" "Bray", "Odesa", 35, True, 267822],
-    ["Alex" "Bush", "Kyiv", 65, False, 27271],
-    ["Olga" "Butterfly", "", 22, False, 132452],
+    ["Alex", "Bush", "Odesa", 35, True, 12131],
+    ["Alex", "Bush", "Kyiv, Lobanovskoho str", 65, False, 36738],
+    ["Petr", "Bray", "Odesa", 35, True, 46736],
+    ["Alex", "Bush", "Kyiv", 65, False, 36738],
+    ["Alex", "Bush", "KYIV", 35, False, 366772],
+    ["Petr", "Bray", "Odesa", 35, True, 267822],
+    ["Alex", "Bush", "Kyiv", 65, False, 27271],
+    ["Olga", "Butterfly", "", 22, False, 132452],
 ]
 
 # all married people
-for person in people:
-    # if person[4] is True:
-    if person[4]:
-        print(person)
-
-
 # not married from Kyiv
 # average age of married people
 # 3 Alex below 22
+all_married_people = []
+not_married_from_city = []
+city = "Kyiv"
+
+########
+for person in people:
+    # person: ['Alex', 'Bush', 'Odesa', 35, True, 12131]
+    name, surname, address, age, is_married, inn = person
+
+    # is_married = person[4]
+    # address = person[2].lower()
+    # address = address.lower()
+    if is_married:
+        # print(person)
+        all_married_people.append(person)
+    if not is_married and city.lower() in address.lower():
+        not_married_from_city.append(person)
+
+print("all married")
+pprint(all_married_people)
+print(f"not married from {city}")
+pprint(not_married_from_city)
+
+
+###########
+
+# all_married_people = []
+# for person in people:
+#     is_married = person[4]
+#     # if person[4] is True:
+#     if is_married:
+#         print(person)
+#         all_married_people.append(person)
+#
+# # not married from Kyiv
+# not_married_from_city = []
+# city = "Kyiv"
+# for person in people:
+#     is_married = person[4]
+#     # if person[4] is True:
+#     address = person[2].lower()
+#     # if not is_married and city == 'Kyiv':
+#     if not is_married and city.lower() in address:
+#         not_married_from_city.append(person)
+
 
 pass
